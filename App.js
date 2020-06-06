1bf89f8d90318130bc966f68829e9dfeaabfc2d2
hieu <18520734@gm.uit.edu.vn>
2020-06-06 16:20:42 +0700

    UPDATE App.js

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import OrdersHistory from './src/Components/Menu/OrdersHistory.js';
-import Auth from './src/Components/Auth/Auth.js';
+import { StyleSheet, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
+import TabNavigator from './src/Components/Main/TabNavigator.js';
+//import OrdersHistory from './src/Components/Menu/OrdersHistory.js';
+//import Auth from './src/Components/Main/Contact/Contact.js';       
+
+StatusBar.setHidden(true);
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Halo!</Text>
-    // </View>
-    <Auth/>
-  );
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
+   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import OrdersHistory from './src/Components/Menu/OrdersHistory.js';
+// import Auth from './src/Components/Auth/Auth.js';
+
+// export default function App() {
+//   return (
+//     // <View style={styles.container}>
+//     //   <Text>Halo!</Text>
+//     // </View>
+//     <Auth/>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
