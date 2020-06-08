f6be1a647a24865c00371be82603b87d938a52ab
hieu <18520734@gm.uit.edu.vn>
2020-06-08 23:03:05 +0700

    UPDATE file JSON and App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, StatusBar } from 'react-native';
+import { StatusBar,Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+
+import MenuDrawer from './src/Components/Main/MenuDrawer.js';
 
 import TabNavigator from './src/Components/Main/TabNavigator.js';
 //import OrdersHistory from './src/Components/Menu/OrdersHistory.js';
@@ -10,30 +12,8 @@ StatusBar.setHidden(true);
 export default function App() {
   return (
     <NavigationContainer>
-      <TabNavigator />
+      <MenuDrawer />
     </NavigationContainer>
    );
 }
 
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import OrdersHistory from './src/Components/Menu/OrdersHistory.js';
-// import Auth from './src/Components/Auth/Auth.js';
-
-// export default function App() {
-//   return (
-//     // <View style={styles.container}>
-//     //   <Text>Halo!</Text>
-//     // </View>
-//     <Auth/>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
