c2634ee4158f57079a9a62f4b2fbf35ced9a1c06
khanhchuong <chuongtangkhanh2104@gmail.com>
2020-06-09 10:52:37 +0700

    replace react-native-swiper-flatlist by custom component

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { StyleSheet, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
-import TabNavigator from './src/Components/Main/TabNavigator.js';
+import TabNavigator from "./src/Components/Main/TabNavigator.js";
 //import OrdersHistory from './src/Components/Menu/OrdersHistory.js';
-//import Auth from './src/Components/Main/Contact/Contact.js';       
+//import Auth from './src/Components/Main/Contact/Contact.js';
 
 StatusBar.setHidden(true);
-export default function App() {
-  return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
-   );
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    );
+  }
 }
 
 // import React from 'react';
--- src/Components/Main/Products/Products.js
+++ src/Components/Main/Products/Products.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ProductsView from './ProductsView';
-import ProductDetail from './ProductDetail';
+import React from "react";
+import {} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ProductsView from "./ProductsView";
+import ProductDetail from "./ProductDetail";
 
 const Stack = createStackNavigator();
 
 export default class ProductsScreen extends React.Component {
   render() {
     return (
-      <Stack.Navigator initialRouteName="ProductsView" >
-        <Stack.Screen 
-          name='ProductDetail' 
-          component={ProductDetail} 
-          options={{ headerShown: false }} 
+      <Stack.Navigator initialRouteName="ProductsView">
+        <Stack.Screen
+          name="ProductDetail"
+          component={ProductDetail}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
-         name='ProductsView' 
-         component={ProductsView} 
-         options={{ headerShown: false }} 
-         navigation={this.props.navigation} 
+          name="ProductsView"
+          component={ProductsView}
+          options={{ headerShown: false }}
+          navigation={this.props.navigation}
         />
       </Stack.Navigator>
     );
